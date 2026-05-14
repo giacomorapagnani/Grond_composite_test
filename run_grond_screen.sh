00d7daf44ad671111be51a1ef46319dbc1ef958d
giacomorapagnani <g.rapagnani@studenti.unipi.it>
2026-05-14 19:19:15 +0200

    v3

---
+++ run_grond_screen.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+events=(
+  "flegrei_2018_09_18_21_36_41"
+  "flegrei_2023_06_11_06_44_25"
+  "flegrei_2023_09_07_17_45_28"
+  "flegrei_2023_09_26_07_10_29"
+  "flegrei_2023_10_02_20_08_26"
+  "flegrei_2024_04_27_03_44_56"
+  "flegrei_2024_05_22_06_28_00"
+  "flegrei_2024_06_08_01_52_04"
+  "flegrei_2024_06_18_01_58_24"
+  "flegrei_2024_07_26_11_46_21"
+  "flegrei_2024_08_30_19_23_15"
+  "flegrei_2025_02_16_14_30_02"
+  "flegrei_2025_03_13_00_25_02"
+  "flegrei_2025_03_14_18_44_10"
+)
+
+for ev in "${events[@]}"; do
+  gronf_name=$(echo "$ev" | sed 's/flegrei_/flegrei./; s/_/./g; s/$/.composite.LF.std.gronf/')
+  screen -dmS "$ev" bash -c "grond go CONFIG/${gronf_name} ${ev} && grond report runs/cmt_composite_LF_std_${ev}"
+  echo "Avviato: $ev"
+done
